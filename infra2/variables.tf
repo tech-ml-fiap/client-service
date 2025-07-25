d3a17e7242ce087cd6807b6684bfa0f67076b1e0
luanliduario <luanliduario@gmail.com>
2025-07-25 02:00:44 -0300

    deploy fix

--- infra2/variables.tf
+++ infra2/variables.tf
@@ -1,16 +1,81 @@
-variable "aws_region"           { type = string default = "us-east-1" }
-variable "ecr_repo_name"        { type = string default = "clientservice" }
 
-variable "cluster_name"         { type = string default = "clientservice-cluster" }
-variable "service_name"         { type = string default = "clientservice-svc" }
-variable "container_port"       { type = number default = 8000 }
-
-# DB connection (injected via secrets)
-variable "db_host"              { type = string }
-variable "db_port"              { type = string }
-variable "db_user"              { type = string }
-variable "db_password"          { type = string }
-variable "db_name"              { type = string }
-
-# IAM execution role pré‑existente
 variable "execution_role_arn"   { type = string }
+
+variable "aws_region" {
+  description = "Região AWS onde os recursos serão criados"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "db_host" {
+  description = "Endpoint do RDS"
+  type        = string
+}
+
+variable "db_port" {
+  description = "Porta do RDS"
+  type        = string
+  default     = "5432"
+}
+
+variable "db_user" {
+  description = "Usuário do banco"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Senha do banco"
+  type        = string
+}
+
+variable "db_name" {
+  description = "Nome do schema/banco"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Nome do cluster ECS"
+  type        = string
+  default     = "clientservice-cluster"
+}
+
+variable "service_name" {
+  description = "Nome do ECS Service e da Task Definition"
+  type        = string
+  default     = "clientservice-svc"
+}
+
+variable "container_port" {
+  description = "Porta exposta pelo container"
+  type        = number
+  default     = 8000
+}
+
+variable "ecr_repo_name" {
+  description = "Nome do repositório ECR"
+  type        = string
+  default     = "clientservice"
+}
+
+variable "image_uri" {
+  type        = string
+}
+
+variable "cpu" {
+  type        = number
+  default         = 256
+}
+
+variable "memory" {
+  type        = number
+  default         = 512
+}
+
+variable "execution_role" {
+  type        = string
+  default = "arn:aws:iam::049015295261:role/LabRole"
+}
+variable "exec_role_name" {
+  description = "Nome da LabRole existente (padrão do AWS Academy)"
+  type        = string
+}
