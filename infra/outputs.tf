2b29ad4481984267cb0797b2354e6f7fbfc388ec
luanliduario <luanliduario@gmail.com>
2025-07-25 01:31:12 -0300

    deploy fix

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,8 +1,8 @@
-output "alb_dns" {
-  description = "URL pública da aplicação"
-  value       = "http://${aws_lb.app.dns_name}"
-}
+output "cluster_name"        { value = aws_ecs_cluster.this.name }
+output "service_name"        { value = aws_ecs_service.this.name }
+output "task_definition_arn" { value = aws_ecs_task_definition.this.arn }
 
-output "cluster_name"            { value = aws_ecs_cluster.this.name }
-output "service_name"            { value = var.service_name }
-output "ecs_task_definition_arn" { value = aws_ecs_task_definition.app.arn }
+output "default_subnets" { value = jsonencode(data.aws_subnets.default.ids) }
+output "default_sg"      { value = data.aws_security_group.default.id }
+
+output "alb_dns_name"    { value = aws_lb.this.dns_name }
